f42a7259989162e5a1dd2cc4c87071953f071206
ramana1443 <25b21ds014@adityauniversity.in>
2025-10-16 15:51:32 +0530

    Delete arrays.py

--- arrays.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-arr1 = np.array([1,2,3,4])
-arr2 = np.array([4,3,2,1])
-print("array 1", arr1)
-print("array 2", arr2)
-print("addition", np.add(arr1, arr2))
-print("all > 0 in arr1:",np.all(arr1>0))
-print("arr1 > arr2:",np.all(arr2>0))
