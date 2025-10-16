10442a792d665bf76169a8ba9212359100fe9867
ramana1443 <25b21ds014@adityauniversity.in>
2025-10-16 16:06:28 +0530

    Add files via upload

--- factorial of a given number.cpp
+++ factorial of a given number.cpp
@@ -4,7 +4,7 @@ int factorial (int n) {
     if (n == 0 || n == 1) 
         return 1; 
     else 
-        return n * factorial (n - 1) ;  // Recursive call  
+        return n * factorial (n - 1) ; 
 } 
 int main () { 
     int number; 
